22a8f2ad7f338da61287e22dd31d0e64e59edf86
jporras1 <javi.jr2150@gmail.com>
2019-10-16 13:07:01 -0500

    created labels to test the rotation of a view. Added an extension to UIView that easily rotates, both in radians or in degrees.

--- sidewaysLabels/ViewController.swift
+++ sidewaysLabels/ViewController.swift
@@ -12,9 +12,54 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = .gray
+        setupViews()
+        label1.transform = CGAffineTransform(rotationAngle: CGFloat.pi / 2)
+        label2.rotate(degrees: 180)
     }
+    
 
+    let label1: UILabel = {
+       let label = UILabel()
+        label.text = "Sample Text"
+        label.textAlignment = .center
+        label.backgroundColor = .white
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let label2: UILabel = {
+        let label = UILabel()
+        label.text = "Other Sample Text"
+        label.textAlignment = .center
+        label.backgroundColor = .yellow
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
 
+    func setupViews(){
+        view.addSubview(label1)
+        view.addSubview(label2)
+        
+        label1.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        label1.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        label1.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        label1.heightAnchor.constraint(equalToConstant: 30).isActive = true
+        
+        label2.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        label2.topAnchor.constraint(equalTo: label1.bottomAnchor, constant: 40).isActive = true
+        label2.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        label2.heightAnchor.constraint(equalToConstant: 30).isActive = true
+    }
+}
+
+extension UIView {
+    func rotate(degrees: CGFloat) {
+        rotate(radians: CGFloat.pi * degrees / 180.0)
+    }
+    
+    func rotate(radians: CGFloat) {
+        self.transform = CGAffineTransform(rotationAngle: radians)
+    }
 }
 
