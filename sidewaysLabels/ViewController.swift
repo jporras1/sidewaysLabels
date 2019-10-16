ee92ae6841b1e5ce747b16e239c6ba992bd162d7
jporras1 <javi.jr2150@gmail.com>
2019-10-16 12:50:44 -0500

    Initial Commit

---
+++ sidewaysLabels/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sidewaysLabels
+//
+//  Created by Javier Porras jr on 10/16/19.
+//  Copyright © 2019 Javier Porras jr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
